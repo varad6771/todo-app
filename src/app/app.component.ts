ec89c029e07856783c7fd52169b6cb34830ed2a5
Varad Vanjape <varadtech67@gmail.com>
2019-03-10 21:25:44 +0530

    basic todo app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todoArray = []
+
+  addTodo(value1) {
+    console.log("insert item")
+    this.todoArray.push(value1);
+    console.log(this.todoArray)
+  }
+  deleteItem(value) {
+    console.log("delete item")
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (value == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+  }
 }
